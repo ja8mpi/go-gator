4d6e7ef4a62363ec5bb165774dcd78e20fb8c184
ja8mpi <ja8mpi@inf.elte.hu>
2025-07-09 07:25:59 +0200

    installed sqlc, added migrations and queries, added postgres driver and started register flow

--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module github.com/ja8mpi/go-gator
 
 go 1.24.4
 
-require github.com/ja8mpi/go-gator-config v0.0.0-20250708190433-339414a31cf9 // indirect
+require (
+	github.com/google/uuid v1.6.0 // indirect
+	github.com/ja8mpi/go-gator-config v0.0.0-20250708190433-339414a31cf9 // indirect
+	github.com/lib/pq v1.10.9 // indirect
+)
--- main.go
+++ main.go
@@ -1,15 +1,90 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
+	"os"
+
+	_ "github.com/lib/pq"
 
 	config "github.com/ja8mpi/go-gator-config"
+	"github.com/ja8mpi/go-gator/internal/database"
 )
 
+type state struct {
+	db  *database.Queries
+	cfg *config.Config
+}
+
+type command struct {
+	name      string
+	arguments []string
+}
+
+type commands struct {
+	cmds map[string]func(*state, command) error
+}
+
+func (c *commands) run(s *state, cmd command) error {
+	com, exists := c.cmds[cmd.name]
+	if !exists {
+		return fmt.Errorf("command does not exist")
+	}
+	return com(s, cmd)
+}
+
+func (c *commands) register(name string, f func(*state, command) error) {
+	if c.cmds == nil {
+		c.cmds = make(map[string]func(*state, command) error)
+	}
+	c.cmds[name] = f
+}
+
+func handlerLogin(s *state, cmd command) error {
+	if len(cmd.arguments) <= 0 {
+		return fmt.Errorf("no arguments provided")
+	}
+	s.cfg.SetUser(cmd.arguments[0])
+	fmt.Println("The user has been set")
+	return nil
+}
+
 func main() {
+
 	var cfg config.Config
 	cfg.Read()
-	cfg.SetUser("daniel")
-	cfg.Read()
-	fmt.Printf("%v\n%v\n", cfg.DBUrl, cfg.CurrentUserName)
+	configState := state{
+		cfg: &cfg,
+	}
+
+	// connect to db
+	db, err := sql.Open("postgres", cfg.DBUrl)
+	dbQueries := database.New(db)
+
+	coms := commands{
+		cmds: make(map[string]func(*state, command) error),
+	}
+	coms.register("login", handlerLogin)
+
+	args := os.Args[1:]
+	if len(args) == 0 {
+		fmt.Println("Usage: <command> [arguments...]")
+		os.Exit(1)
+		return
+	}
+
+	commandName := args[0]
+	commandsArgs := args[1:]
+
+	cmd := command{
+		name:      commandName,
+		arguments: commandsArgs,
+	}
+
+	err := coms.run(&configState, cmd)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	os.Exit(0)
 }
